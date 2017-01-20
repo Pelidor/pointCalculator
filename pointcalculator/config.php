2e320de833b48f5221ec1550c8f354e12ae9e5f6
Stephan Hauff <stephan.hauff@wabsolute.de>
2017-01-20 18:00:41 +0100

    moved

--- pointcalculator/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'calculator';
-$password = 'XiHbZ6r0';
-$database = 'pointCalculator';
-
-$pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-
